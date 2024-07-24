086ec33ac5aac8a1dcb9a8ead4f4d9bcb4cfc24c
selmarmdhschool <selmarmdhschool@gmail.com>
2024-07-24 07:24:09 +0000

    First commit

---
+++ lib/hal_komputer.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Komputer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            Padding(padding: EdgeInsets.all(20.0),),
+            Text("Komputer", style: TextStyle(fontSize: 30.0),
+            ),
+          Padding(padding: EdgeInsets.all(20.0),),
+            Icon(
+              Icons.computer,
+              size: 90.0,
+            )
+          ],
+      ),
+    )
+    );
+  }
+}
---
+++ lib/hal_radio.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Radio extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            Padding(padding: EdgeInsets.all(20.0),),
+            Text("Radio", style: TextStyle(fontSize: 30.0),
+            ),
+          Padding(padding: EdgeInsets.all(20.0),),
+            Icon(
+              Icons.radio,
+              size: 90.0,
+            )
+          ],
+      ),
+    )
+    );
+  }
+}
